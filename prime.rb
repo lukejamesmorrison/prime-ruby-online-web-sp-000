49645349622c2e9f393469055899562df3a43ff2
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 14:11:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,6 @@ def prime?(number)
   # a prime must be greater than 2
   # odds cannot be prime
   # odds can only divide by themselves - start with 2 and 3
-  
   if number <= 3 
     return number > 1
   elsif number % 2 == 0 || number % 3 == 0
@@ -20,17 +19,4 @@ def prime?(number)
   true
 end
 
-
-# if n ≤ 3 then
-#         return n > 1
-#     else if n mod 2 = 0 or n mod 3 = 0
-#         return false
-
-#     let i ← 5
-
-#     while i × i ≤ n do
-#         if n mod i = 0 or n mod (i + 2) = 0
-#             return false
-#         i ← i + 6
-
-#     return true
+# https://en.wikipedia.org/wiki/Primality_test
