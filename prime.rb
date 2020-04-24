bb12ad3532fd78f670eb66b25457449a272f520b
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 14:00:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   # a prime must be greater than 1
   # odds cannot be prime
   # odds can only divide by themselves - use base 3
-  if (number < 2 || number.even? || number % 3 != 0 ) && number != 2
+  if (number < 2 || number.even? || number % 3 != 0) && number != 2
     return false
   end
   
