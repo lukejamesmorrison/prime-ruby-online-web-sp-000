7c6b04879ea431291506b66c8c7d02b565013004
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 13:54:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 def prime?(number)
-  # odds cannot be prime
+  
   # odds can only divide by themselves
   
+  # odds cannot be prime
   if number.even? do
     return false
   end
+  
   
   
   
